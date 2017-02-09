164b7bbda051104ef55ec5e2bc49f17f588b12b6
marcosmc4 <marcosmc4@gmail.com>
2017-02-09 20:19:23 +0100

    Added user profile partial view and angujar js controller

---
+++ src/main/resources/static/angularjs/learner/user_profile_controller.js
@@ -0,0 +1,12 @@
+/**
+ * Created by MARCOS on 08/02/2017.
+ */
+/**
+ * Created by MARCOS on 01/02/2017.
+ */
+
+app.controller('userProfileCtrl', ['$scope', '$http', function($scope, $http) {
+
+
+
+}]);
